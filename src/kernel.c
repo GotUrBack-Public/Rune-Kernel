a134ff10ffc63223f6f5fa1c8d58178028c82cf4
GotUrBack04 <75336897532579754357a@gmail.com>
2026-05-23 22:41:56 +0200

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,1 +1,16 @@
+#include <stdint.h>
+#include <stddef.h>
 
+static volatile struct {
+    uint64_t id[4];
+    uint64_t revision;
+} stack_request = {
+    .id = {0, 0, 0, 0},
+    .revision = 0
+};
+
+void kmain(void) {
+    for (;;) {
+        __asm__("hlt");
+    }
+}
