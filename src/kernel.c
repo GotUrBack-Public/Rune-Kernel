c2062a4963b7b74bc10e4d4e74b029dbd0b23580
GotUrBack04 <75336897532579754357a@gmail.com>
2026-05-24 02:56:41 +0200

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,19 +1,30 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
-__attribute__((used, section(".requests")))
-static volatile uint64_t limine_requests[4] = {
+__attribute__((used, section(".limine_requests")))
+static volatile uint64_t limine_requests[] = {
     0xc7b1dd30df4c8b88, 0x0a82e88345a78616,
-    0x2c139c892b1574e0, 0x489868f0376e108d
+    0x2c139c892b1574e0, 0x489868f0376e108d,
+    0, 0
 };
 
-void kmain(void) {
+__attribute__((used))
+void _start(void) {
+    
     volatile char *video_memory = (volatile char *)0xffffffff800b8000;
     
-    video_memory[0] = 'R'; video_memory[1] = 0x0F;
-    video_memory[2] = 'U'; video_memory[3] = 0x0F;
-    video_memory[4] = 'N'; video_memory[5] = 0x0F;
-    video_memory[6] = 'E'; video_memory[7] = 0x0F;
+    for (int i = 0; i < 80 * 25 * 2; i += 2) {
+        video_memory[i] = ' ';
+        video_memory[i+1] = 0x07;
+    }
+
+    const char *msg = "RUNE KERNEL ONLINE";
+    for (int i = 0; msg[i] != '\0'; i++) {
+        video_memory[i * 2] = msg[i];
+        video_memory[i * 2 + 1] = 0x0F;
+    }
+
 
     for (;;) {
         __asm__("hlt");
