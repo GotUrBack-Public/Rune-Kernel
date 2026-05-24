58d3c71def777e0fc01b878b267140c97324508b
GotUrBack04 <75336897532579754357a@gmail.com>
2026-05-24 02:38:15 +0200

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,22 +1,19 @@
 #include <stdint.h>
 #include <stddef.h>
 
-static volatile uint64_t limine_requests[] = {
+__attribute__((used, section(".requests")))
+static volatile uint64_t limine_requests[4] = {
     0xc7b1dd30df4c8b88, 0x0a82e88345a78616,
-    0x2c139c892b1574e0, 0x489868f0376e108d,
-    0
+    0x2c139c892b1574e0, 0x489868f0376e108d
 };
 
 void kmain(void) {
-    
     volatile char *video_memory = (volatile char *)0xffffffff800b8000;
     
-    video_memory[0] = 'R';
-    video_memory[1] = 0x0F;
-    video_memory[2] = 'U';
-    video_memory[3] = 0x0F;
-    video_memory[4] = 'N';
-    video_memory[5] = 0x0F;
+    video_memory[0] = 'R'; video_memory[1] = 0x0F;
+    video_memory[2] = 'U'; video_memory[3] = 0x0F;
+    video_memory[4] = 'N'; video_memory[5] = 0x0F;
+    video_memory[6] = 'E'; video_memory[7] = 0x0F;
 
     for (;;) {
         __asm__("hlt");
